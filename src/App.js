3d01a789a3c07edd4aeb765004c714518a848254
Malik Kamal <almaqdad55@gmail.com>
2021-10-21 21:32:17 +1030

    Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
 import NavBar from "./components/NavBar";
-
+import Header from "./components/Header";
 //Styles
 import "./App.scss";
 import "bootstrap/dist/css/bootstrap.min.css";
+import React from "react";
+import Particles from "react-particles-js";
 
 const App = () => {
-  return <NavBar />;
+  return (
+    <React.Fragment>
+      <Particles
+        className="particles-canvas"
+        params={{
+          particles: {
+            number: {
+              value: 30,
+              density: {
+                enable: true,
+              },
+            },
+            shape: {
+              type: "circle",
+              stroke: {
+                width: 6,
+                color: "#f9ab00",
+              },
+            },
+          },
+        }}
+      />
+      <NavBar />
+      <Header />
+    </React.Fragment>
+  );
 };
 
 export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Typed from "react-typed";
+
+const Header = () => {
+  return (
+    <div className="header-wrapper">
+      <div className="main-info">
+        <h1>Web development and web promo</h1>
+        <Typed
+          className="typed-text"
+          strings={["Web designs", "Web Developement", "Front-end Developer"]}
+          typeSpeed={80}
+          backSpeed={50}
+          loop
+        />
+        <a href="#" className="btn-main-offer">
+          contact me
+        </a>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
